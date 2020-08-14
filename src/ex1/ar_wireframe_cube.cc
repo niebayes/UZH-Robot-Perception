88ffcfe6ef8b38bbe8dac11e86d737ee5cbfd0a8
niebayes <niebayes@gmail.com>
2020-08-14 14:31:40 +0800

    Complete ex1 part I

--- src/ex1/ar_wireframe_cube.cc
+++ src/ex1/ar_wireframe_cube.cc
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <optional>  // C++17: std::optional
 #include <sstream>
 #include <string>
 #include <vector>
@@ -14,21 +15,72 @@
 
 using RigidTransformation = Eigen::Matrix<double, 3, 4>;
 
+// @brief Load 6dof poses from text file and store them into a 2d vector.
 static std::vector<std::vector<double>> LoadPoses(
     const std::string& file_name) {
   std::vector<std::vector<double>> poses;
 
   std::ifstream fin(file_name, std::ios::in);
-  std::string pose;
-  while (std::getline(fin, pose)) {
-    double w_x, w_y, w_z, t_x, t_y, t_z;
-    std::istringstream iss(pose);
-    if (iss.good() && iss >> w_x >> w_y >> w_z >> t_x >> t_y >> t_z) {
-      poses.push_back(std::vector<double>{w_x, w_y, w_z, t_x, t_y, t_z});
+  if (fin.is_open()) {
+    std::string pose;
+    while (std::getline(fin, pose)) {
+      double w_x, w_y, w_z, t_x, t_y, t_z;
+      std::istringstream iss(pose);
+      if (iss.good() && iss >> w_x >> w_y >> w_z >> t_x >> t_y >> t_z) {
+        poses.push_back(std::vector<double>{w_x, w_y, w_z, t_x, t_y, t_z});
+      }
     }
+    fin.close();
+    LOG(INFO) << "Loaded " << poses.size() << " poses";
+  } else {
+    LOG(ERROR) << "Fail loading file " << file_name;
   }
-  LOG(INFO) << "Load " << poses.size() << " poses";
   return poses;
+}
+
+//@brief Load camera calibration matrix K and store it into a vector.
+static std::vector<double> LoadK(const std::string& file_name) {
+  std::vector<double> K;
+
+  std::ifstream fin(file_name, std::ios::in);
+  if (fin.is_open()) {
+    std::string row;
+    while (std::getline(fin, row)) {
+      double coeff_1, coeff_2, coeff_3;
+      std::istringstream iss(row);
+      if (iss.good() && iss >> coeff_1 >> coeff_2 >> coeff_3) {
+        K.push_back(coeff_1);
+        K.push_back(coeff_2);
+        K.push_back(coeff_3);
+      }
+    }
+    fin.close();
+  } else {
+    LOG(ERROR) << "Fail loading file " << file_name;
+  }
+  return K;
+}
+
+//@brief Load lens distortion coefficients D and store them into a vector.
+static std::vector<double> LoadD(const std::string& file_name) {
+  std::vector<double> D;
+
+  std::ifstream fin(file_name, std::ios::in);
+  if (fin.is_open()) {
+    std::string coeffs;
+    if (std::getline(fin, coeffs)) {
+      double k1, k2;
+      std::istringstream iss(coeffs);
+      if (iss.good() && iss >> k1 >> k2) {
+        D.push_back(k1);
+        D.push_back(k2);
+      }
+    }
+    fin.close();
+  } else {
+    LOG(ERROR) << "Fail loading file " << file_name;
+  }
+  return D;
 }
 
 //@brief Imitate matlab's meshgrid.
@@ -87,15 +139,22 @@ enum DistorionCoefficientIndices : int { INDEX_RADIAL_K1, INDEX_RADIAL_K2 };
 
 //@brief Distort normalized image points to get the image points expressed in
 // pixel coords.
+// Distortion model: x_d = x_u * (1 + k1 * r2 + k2 * r2 * r2)
 static void DistortPoints(
     const Eigen::Ref<const Eigen::Matrix2Xd>& normalized_image_points,
     Eigen::Matrix2Xd* distorted_image_points,
-    const Eigen::Ref<const Eigen::VectorXd>& D) {
+    const Eigen::Ref<const Eigen::Vector2d>& D) {
   // Unpack D to get the distortion coefficients
   const double k1 = D(INDEX_RADIAL_K1);
   const double k2 = D(INDEX_RADIAL_K2);
 
   const Eigen::VectorXd r2 = normalized_image_points.colwise().squaredNorm();
+  const Eigen::VectorXd distortion_factor =
+      (k1 * r2 + k2 * r2.cwiseProduct(r2)).unaryExpr([](double x) {
+        return ++x;
+      });
+  *distorted_image_points =
+      normalized_image_points * distortion_factor.asDiagonal();
 }
 
 //@brief Project modes used in ProjectPoints.
@@ -107,25 +166,52 @@ enum ProjectModes : int { PROJECT_WITH_DISTORTION, PROJECT_WITHOUT_DISTORTION };
 
 //@brief Project 3D scene points according to calibration matrix K and optional
 // user provided distortion coefficients D
-template <typename Derived, typename... Args>
+//! The std::optional is a feature of C++17. Alternatively you can avoid this by
+//! overloading ProjectPoints function.
+//? This paradigm is nor working.
+// template <typename Derived>
+// const Eigen::Ref<const Eigen::MatrixBase<Derived>>& object_points
 static void ProjectPoints(
-    const Eigen::Ref<const Eigen::MatrixBase<Derived>>& object_points,
+    const Eigen::Ref<const Eigen::Matrix3Xd>& object_points,
     Eigen::Matrix2Xd* image_points, const Eigen::Ref<const Eigen::Matrix3d>& K,
-    int project_mode, Args... D) {
-  // w
+    int project_mode, std::optional<Eigen::Vector2d> D_opt = std::nullopt) {
+  Eigen::Vector2d D = Eigen::Vector2d::Zero();
+  if (D_opt) {
+    D = D_opt.value();
+  }
+  const Eigen::Matrix2Xd normalized_image_points =
+      object_points.colwise().hnormalized();
+  DistortPoints(normalized_image_points, image_points, D);
+  *image_points = (K * image_points->colwise().homogeneous()).topRows(2);
+}
+
+//@brief Imitate matlab's scatter.
+static void Scatter(cv::InputOutputArray image,
+                    const Eigen::Ref<const Eigen::VectorXi>& x,
+                    const Eigen::Ref<const Eigen::VectorXi>& y, int radius,
+                    const cv::Scalar& color, int thickness = 1) {
+  if (x.size() <= 0 || y.size() <= 0) {
+    LOG(ERROR) << "Invalid input vectors";
+    return;
+  } else if (x.size() != y.size()) {
+    LOG(ERROR)
+        << "YOU_MIXED_DIFFERENT_SIZED_VECTORS";  // Mimic eigen's behavior
+    return;
+  }
+
+  const int kNumPoints = x.size();
+  for (int i = 0; i < kNumPoints; ++i) {
+    cv::circle(image, {x(i), y(i)}, radius, color, thickness);
+  }
+  LOG(INFO) << "Created a scatter plot with " << kNumPoints
+            << " points rendered";
 }
 
 int main(int /*argc*/, char** argv) {
   google::InitGoogleLogging(argv[0]);
   google::LogToStderr();
 
-  const std::string kFilePath{"data/ex1/images/"};
-  const int kImageIndex = 1;
-  const std::string kFileName{
-      cv::format((kFilePath + "img_%04d.jpg").c_str(), kImageIndex)};
-  cv::Mat image = cv::imread(kFileName, cv::IMREAD_GRAYSCALE);
-  const std::vector<std::vector<double>> poses =
-      LoadPoses("data/ex1/poses.txt");
+  const std::string kFilePath{"data/ex1/"};
 
   // Construct a 2D mesh grid where each vertix corrsponds to a inner corner.
   // Given settings, cf. data/ex1/images/img_0001.jpg
@@ -144,16 +230,50 @@ int main(int /*argc*/, char** argv) {
   //! rows and cols for checking. Hence, before manipulating
   //! the rows/cols/coefficients of a dynamic size matrix, be
   //! aware of initializing it first.
-  Eigen::MatrixXd p_W_corners(3, kNumCorners);
+  Eigen::Matrix3Xd p_W_corners(3, kNumCorners);
   p_W_corners.row(0) = X.cast<double>();
   p_W_corners.row(1) = Y.cast<double>();
   p_W_corners.row(2).setZero();
   p_W_corners *= kCellSize;
 
   // Rigid transformation from world coord. to camera coord.
+  const std::vector<std::vector<double>> poses =
+      LoadPoses(kFilePath + "poses.txt");
   RigidTransformation T_C_W;
   PoseVectorToTransformationMatrix(poses[0], &T_C_W);
-  Eigen::MatrixXd p_C_corners = T_C_W * p_W_corners.colwise().homogeneous();
+  const Eigen::Matrix3Xd p_C_corners =
+      T_C_W * p_W_corners.colwise().homogeneous();
+
+  // Project 3D corners to image plane
+  // Load K and D
+  //@warning This is cool, but also dangerous! Because the Eigen object will NOT
+  // create its own memory. It will operate on the memory provided by "data". In
+  // other words, working with the Eigen object when the "data" object is out of
+  // scope will result in a segmentation fault (or memory access violation)
+  //! Hence you shall not use const qualifier to K_tmp, otherwise errors
+  //! induced.
+  std::vector<double> K_tmp = LoadK(kFilePath + "K.txt");
+  std::vector<double> D_tmp = LoadD(kFilePath + "D.txt");
+  //@warning Eigen use column-major storage order! Hence when constructing a
+  // eigen Matrix use the data pointer of an external array-like object, be sure
+  // transposing it!
+  const Eigen::Matrix3d K =
+      (Eigen::Map<Eigen::Matrix3d>(K_tmp.data())).transpose();
+  const Eigen::Vector2d D(D_tmp.data());
+
+  Eigen::Matrix2Xd image_points;
+  ProjectPoints(p_C_corners, &image_points, K, PROJECT_WITH_DISTORTION, D);
+
+  // Superimpose points on the image
+  const int kImageIndex = 1;
+  const std::string kImageName{
+      cv::format((kFilePath + "images/img_%04d.jpg").c_str(), kImageIndex)};
+  cv::Mat image = cv::imread(kImageName, cv::IMREAD_COLOR);
+  const Eigen::VectorXi& x = image_points.row(0).cast<int>();
+  const Eigen::VectorXi& y = image_points.row(1).cast<int>();
+  Scatter(image, x, y, 3, {0, 0, 255}, cv::FILLED);
+  cv::imshow("", image);
+  cv::waitKey(0);
 
   return EXIT_SUCCESS;
 }
