168adbc13bfeaeb17cdad588e5989d56a2a8d618
niebayes <niebayes@gmail.com>
2020-09-04 14:59:43 +0800

    Ultimately complete ex7

--- src/ex7/localizing_with_ransac.cc
+++ src/ex7/localizing_with_ransac.cc
@@ -11,7 +11,7 @@
 #include "io.h"
 #include "matlab_port.h"
 #include "opencv2/opencv.hpp"
-// #include "pcl/visualization/pcl_plotter.h"
+#include "pcl/visualization/pcl_plotter.h"
 #include "ransac.h"
 #include "transfer.h"
 
@@ -60,76 +60,75 @@ int main(int argc, char** argv) {
   std::tie(best_poly_coeffs, max_inlier_counts) =
       uzh::ParabolaRANSAC(data, max_noise);
 
-  //   // Compare various fits with RANSAC.
-  //   pcl::visualization::PCLPlotter::Ptr plotter(
-  //       new pcl::visualization::PCLPlotter);
-  //   plotter->setTitle("Fitted parabolas");
-  //   plotter->setShowLegend(true);
-  //   plotter->setXRange(x_start, x_start + x_span);
-  //   plotter->setYRange(y_lowest - max_noise, y_highest + max_noise);
+  // Compare various fits with RANSAC.
+  pcl::visualization::PCLPlotter::Ptr plotter(
+      new pcl::visualization::PCLPlotter);
+  plotter->setTitle("Fitted parabolas");
+  plotter->setShowLegend(true);
+  plotter->setXRange(x_start, x_start + x_span);
+  plotter->setYRange(y_lowest - max_noise, y_highest + max_noise);
 
-  //   // Plot scattered data points.
-  //   plotter->addPlotData(arma::conv_to<std::vector<double>>::from(data.row(0)),
-  //                        arma::conv_to<std::vector<double>>::from(data.row(1)),
-  //                        "data points", vtkChart::POINTS);
-  //   // Plot ground truth.
-  //   const arma::vec x_plot =
-  //       arma::linspace<arma::vec>(x_start, x_start + x_span, 100);
-  //   plotter->addPlotData(arma::conv_to<std::vector<double>>::from(x_plot),
-  //                        arma::conv_to<std::vector<double>>::from(
-  //                            arma::polyval(poly_coeffs, x_plot)),
-  //                        "ground truth");
-  //   // Plot full fit with all the data.
-  //   const arma::vec full_fit = arma::polyfit(data.row(0), data.row(1), 2);
-  //   plotter->addPlotData(
-  //       arma::conv_to<std::vector<double>>::from(x_plot),
-  //       arma::conv_to<std::vector<double>>::from(arma::polyval(full_fit,
-  //       x_plot)), "full data fit");
-  //   // Plot RANSAC fitted at each iteration except the last one.
-  //   for (int i = 0; i < best_poly_coeffs.n_cols - 1; ++i) {
-  //     const arma::vec coeffs = best_poly_coeffs.col(i);
-  //     plotter->addPlotData(
-  //         arma::conv_to<std::vector<double>>::from(x_plot),
-  //         arma::conv_to<std::vector<double>>::from(arma::polyval(coeffs,
-  //         x_plot)),
-  //         "");
-  //   }
-  //   // Plot RANSAC fitted result.
-  //   const arma::vec best_coeffs = best_poly_coeffs.tail_cols(1);
-  //   plotter->addPlotData(arma::conv_to<std::vector<double>>::from(x_plot),
-  //                        arma::conv_to<std::vector<double>>::from(
-  //                            arma::polyval(best_coeffs, x_plot)),
-  //                        "RANSAC result");
-  //   plotter->plot();
+  // Plot scattered data points.
+  plotter->addPlotData(arma::conv_to<std::vector<double>>::from(data.row(0)),
+                       arma::conv_to<std::vector<double>>::from(data.row(1)),
+                       "data points", vtkChart::POINTS);
+  // Plot ground truth.
+  const arma::vec x_plot =
+      arma::linspace<arma::vec>(x_start, x_start + x_span, 100);
+  plotter->addPlotData(arma::conv_to<std::vector<double>>::from(x_plot),
+                       arma::conv_to<std::vector<double>>::from(
+                           arma::polyval(poly_coeffs, x_plot)),
+                       "ground truth");
+  // Plot full fit with all the data.
+  const arma::vec full_fit = arma::polyfit(data.row(0), data.row(1), 2);
+  plotter->addPlotData(
+      arma::conv_to<std::vector<double>>::from(x_plot),
+      arma::conv_to<std::vector<double>>::from(arma::polyval(full_fit, x_plot)),
+      "full data fit");
+  // Plot RANSAC fitted at each iteration except the last one.
+  for (int i = 0; i < best_poly_coeffs.n_cols - 1; ++i) {
+    const arma::vec coeffs = best_poly_coeffs.col(i);
+    plotter->addPlotData(
+        arma::conv_to<std::vector<double>>::from(x_plot),
+        arma::conv_to<std::vector<double>>::from(arma::polyval(coeffs, x_plot)),
+        "");
+  }
+  // Plot RANSAC fitted result.
+  const arma::vec best_coeffs = best_poly_coeffs.tail_cols(1);
+  plotter->addPlotData(arma::conv_to<std::vector<double>>::from(x_plot),
+                       arma::conv_to<std::vector<double>>::from(
+                           arma::polyval(best_coeffs, x_plot)),
+                       "RANSAC result");
+  plotter->plot();
 
-  //   // Plot the maximum inlier counts at eack iteration.
-  //   pcl::visualization::PCLPlotter::Ptr inlier_cnt_plotter(
-  //       new pcl::visualization::PCLPlotter);
-  //   inlier_cnt_plotter->setTitle("Maximum inliers count");
-  //   inlier_cnt_plotter->setXTitle("iteration");
-  //   inlier_cnt_plotter->setYTitle("Number of inliers");
-  //   const arma::vec iterations = arma::linspace<arma::vec>(
-  //       1, max_inlier_counts.size(), max_inlier_counts.size());
-  //   inlier_cnt_plotter->addPlotData(
-  //       arma::conv_to<std::vector<double>>::from(iterations),
-  //       arma::conv_to<std::vector<double>>::from(max_inlier_counts),
-  //       "max inliers");
-  //   inlier_cnt_plotter->plot();
+  // Plot the maximum inlier counts at eack iteration.
+  pcl::visualization::PCLPlotter::Ptr inlier_cnt_plotter(
+      new pcl::visualization::PCLPlotter);
+  inlier_cnt_plotter->setTitle("Maximum inliers count");
+  inlier_cnt_plotter->setXTitle("iteration");
+  inlier_cnt_plotter->setYTitle("Number of inliers");
+  const arma::vec iterations = arma::linspace<arma::vec>(
+      1, max_inlier_counts.size(), max_inlier_counts.size());
+  inlier_cnt_plotter->addPlotData(
+      arma::conv_to<std::vector<double>>::from(iterations),
+      arma::conv_to<std::vector<double>>::from(max_inlier_counts),
+      "max inliers");
+  inlier_cnt_plotter->plot();
 
-  //   // Compare the RMS error of the full fit and that of RANSAC fit.
-  //   const arma::vec x_rms = arma::linspace<arma::vec>(0.0, 1.0, 100);
-  //   const double full_fit_rms =
-  //       std::sqrt(uzh::arma2eigen(arma::polyval(poly_coeffs, x_rms) -
-  //                                 arma::polyval(full_fit, x_rms))
-  //                     .squaredNorm() /
-  //                 best_poly_coeffs.n_cols);
-  //   const double ransac_fit_rms =
-  //       std::sqrt(uzh::arma2eigen(arma::polyval(poly_coeffs, x_rms) -
-  //                                 arma::polyval(best_coeffs, x_rms))
-  //                     .squaredNorm() /
-  //                 best_poly_coeffs.n_cols);
-  //   std::cout << "Full fit RMS: " << full_fit_rms << '\n';
-  //   std::cout << "RANSAC fit RMS: " << ransac_fit_rms << '\n';
+  // Compare the RMS error of the full fit and that of RANSAC fit.
+  const arma::vec x_rms = arma::linspace<arma::vec>(0.0, 1.0, 100);
+  const double full_fit_rms =
+      std::sqrt(uzh::arma2eigen(arma::polyval(poly_coeffs, x_rms) -
+                                arma::polyval(full_fit, x_rms))
+                    .squaredNorm() /
+                best_poly_coeffs.n_cols);
+  const double ransac_fit_rms =
+      std::sqrt(uzh::arma2eigen(arma::polyval(poly_coeffs, x_rms) -
+                                arma::polyval(best_coeffs, x_rms))
+                    .squaredNorm() /
+                best_poly_coeffs.n_cols);
+  std::cout << "Full fit RMS: " << full_fit_rms << '\n';
+  std::cout << "RANSAC fit RMS: " << ransac_fit_rms << '\n';
 
   // Part II: localizing with RANSAC.
 
@@ -208,32 +207,31 @@ int main(int argc, char** argv) {
 
   // Show the history of maximum number of inliers and number of iterations at
   // each iteration.
-  // const int num_iterations = num_iterations_history.n_cols;
-  // pcl::visualization::PCLPlotter::Ptr max_num_inliers_plot(
-  //     new pcl::visualization::PCLPlotter);
-  // max_num_inliers_plot->setTitle("Maximum number of inliers");
-  // max_num_inliers_plot->setXTitle("iterations");
-  // max_num_inliers_plot->setShowLegend(true);
-  // max_num_inliers_plot->addPlotData(
-  //     arma::conv_to<std::vector<double>>::from(
-  //         arma::linspace<arma::urowvec>(1, num_iterations, num_iterations)),
-  //     arma::conv_to<std::vector<double>>::from(max_num_inliers_history),
-  //     "inliers", vtkChart::BAR);
-  // max_num_inliers_plot->spinOnce(2000);
-  // max_num_inliers_plot->clearPlots();
+  const int num_iterations = num_iterations_history.n_cols;
+  pcl::visualization::PCLPlotter::Ptr max_num_inliers_plot(
+      new pcl::visualization::PCLPlotter);
+  max_num_inliers_plot->setTitle("Maximum number of inliers");
+  max_num_inliers_plot->setXTitle("iterations");
+  max_num_inliers_plot->setShowLegend(true);
+  max_num_inliers_plot->addPlotData(
+      arma::conv_to<std::vector<double>>::from(
+          arma::linspace<arma::urowvec>(1, num_iterations, num_iterations)),
+      arma::conv_to<std::vector<double>>::from(max_num_inliers_history),
+      "inliers", vtkChart::LINE);
+  max_num_inliers_plot->plot();
 
-  // pcl::visualization::PCLPlotter::Ptr num_iterations_plot(
-  //     new pcl::visualization::PCLPlotter);
-  // num_iterations_plot->setTitle("Number of iterations");
-  // max_num_inliers_plot->setXTitle("iterations");
-  // max_num_inliers_plot->setShowLegend(true);
-  // num_iterations_plot->addPlotData(
-  //     arma::conv_to<std::vector<double>>::from(
-  //         arma::linspace<arma::urowvec>(1, num_iterations, num_iterations)),
-  //     arma::conv_to<std::vector<double>>::from(num_iterations_history),
-  //     "iterations", vtkChart::BAR);
-  // num_iterations_plot->spinOnce(2000);
-  // num_iterations_plot->clearPlots();
+  pcl::visualization::PCLPlotter::Ptr num_iterations_plot(
+      new pcl::visualization::PCLPlotter);
+  num_iterations_plot->setTitle("Estimated number of iterations");
+  max_num_inliers_plot->setXTitle("iterations");
+  max_num_inliers_plot->setShowLegend(true);
+  num_iterations_plot->addPlotData(
+      arma::conv_to<std::vector<double>>::from(
+          arma::linspace<arma::urowvec>(1, num_iterations, num_iterations)),
+      arma::conv_to<std::vector<double>>::from(
+          arma::log10(num_iterations_history)),
+      "iterations", vtkChart::LINE);
+  num_iterations_plot->plot();
 
   // Show all keypoints and all matches.
   std::vector<cv::Mat> imgs_show;
@@ -393,7 +391,8 @@ int main(int argc, char** argv) {
     imgs_show_frame_i.push_back(query_image_show_inlier_outlier_frame_i);
     cv::imshow(winname,
                uzh::MakeCanvas(imgs_show_frame_i, 3 * query_img.rows, 2));
-    cv::waitKey(2000);
+    const char key = cv::waitKey(2000);
+    if (key == 32) cv::waitKey(0);  // 'Space' key -> pause.
   }
 
   return EXIT_SUCCESS;
